9b5fe21871f27d68ed9dc7cc0384bd1df1add922
LordRazen <razen.mailer@gmail.com>
2021-12-07 21:18:57 +0100

    Initial Commit

---
+++ src/main/java/com/minecraftheads/leathercoloriserpro/LeatherColoriserPro.java
@@ -0,0 +1,17 @@
+package com.minecraftheads.leathercoloriserpro;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LeatherColoriserPro extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/minecraftheads/leathercoloriserpro/utils/Logger.java
@@ -0,0 +1,14 @@
+package com.minecraftheads.leathercoloriserpro.utils;
+
+import org.bukkit.Bukkit;
+
+public class Logger {
+    /**
+     * Logger Method
+     *
+     * @param msg
+     */
+    public void info(String msg) {
+        Bukkit.getLogger().info(msg);
+    }
+}
