e4f022d085cea5ed1e49d1d2e81d49aba3959b29
LordRazen <razen.mailer@gmail.com>
2021-12-08 11:24:50 +0100

    Add More GUI Items

--- src/main/java/com/minecraftheads/leathercoloriserpro/LeatherColoriserPro.java
+++ src/main/java/com/minecraftheads/leathercoloriserpro/LeatherColoriserPro.java
@@ -4,8 +4,6 @@ import com.minecraftheads.leathercoloriserpro.commands.CommandLeatherColoriserPr
 import com.minecraftheads.leathercoloriserpro.utils.LCPInventory;
 import com.minecraftheads.leathercoloriserpro.utils.Logger;
 import org.bukkit.Bukkit;
-import org.bukkit.Server;
-import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LeatherColoriserPro extends JavaPlugin {
--- src/main/java/com/minecraftheads/leathercoloriserpro/utils/LCPInventory.java
+++ src/main/java/com/minecraftheads/leathercoloriserpro/utils/LCPInventory.java
@@ -2,6 +2,7 @@ package com.minecraftheads.leathercoloriserpro.utils;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,19 +12,20 @@ import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class LCPInventory implements Listener {
+    private static final String name = "Leather Coloriser Pro (/lcp)";
+    private static final int size = 54;
     private final Inventory inv;
 
     /**
      * Constructor
      */
     public LCPInventory() {
-        inv = Bukkit.createInventory(null, 54, "Leather Coloriser Pro");
-
+        inv = Bukkit.createInventory(null, size, name);
         initializeItems();
     }
 
@@ -31,9 +33,39 @@ public class LCPInventory implements Listener {
      *
      */
     public void initializeItems() {
-        inv.setItem(3, createGuiItem(Material.DIAMOND_SWORD,
-                "Example Sword",
-                "§aFirst line of the lore", "§bSecond line of the lore"));
+        inv.setItem(0,
+                createGuiCustomHeadItem(Material.PLAYER_HEAD,
+                        "Color",
+                        "§aFirst line of the lore",
+                        "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYzY5ZTNlNmU1YjJiOTJmMGJlYjM2OGI3MzhiOTkzZDdiYTIyNWJmOWJiMjc1OGJmYzlmYzJkYWJhNGE1YTdkIn19fQ=="));
+
+        inv.setItem(1,
+                createGuiPlayerHeadItem(Material.PLAYER_HEAD,
+                        "Color",
+                        "§aFirst line of the lore",
+                        "LordRazen=="));
+
+
+        inv.setItem(9,
+                createGuiItem(Material.LEATHER_HORSE_ARMOR,
+                        "Leather Horse Armor",
+                        "§aFirst line of the lore"));
+        inv.setItem(10,
+                createGuiItem(Material.LEATHER_HELMET,
+                        "Leather Helmet",
+                        "§aFirst line of the lore"));
+        inv.setItem(11,
+                createGuiItem(Material.LEATHER_CHESTPLATE,
+                        "Leather Chestplate",
+                        "§aFirst line of the lore"));
+        inv.setItem(12,
+                createGuiItem(Material.LEATHER_LEGGINGS,
+                        "Leather Leggings",
+                        "§aFirst line of the lore"));
+        inv.setItem(13,
+                createGuiItem(Material.LEATHER_BOOTS,
+                        "Leather Boots",
+                        "§aFirst line of the lore"));
 
     }
 
@@ -62,6 +94,60 @@ public class LCPInventory implements Listener {
         return item;
     }
 
+    /**
+     *
+     * @param material
+     * @param name
+     * @param lore
+     * @param playerName
+     * @return
+     */
+    public ItemStack createGuiPlayerHeadItem(final Material material,
+                                       final String name,
+                                       final String lore,
+                                       final String playerName) {
+        // Get Basic Item
+        final ItemStack item = createGuiItem(material, name, lore);
+
+        // Set Player Head Data
+        final SkullMeta meta = (SkullMeta) item.getItemMeta();
+        final OfflinePlayer player = Bukkit.getPlayer(playerName);
+        meta.setOwningPlayer(player);
+        item.setItemMeta(meta);
+        return item;
+    }
+
+
+
+    /**
+     *
+     * @param material
+     * @param name
+     * @param lore
+     * @param value
+     * @return
+     */
+    public ItemStack createGuiCustomHeadItem(final Material material,
+                                             final String name,
+                                             final String lore,
+                                             final String value) {
+        // Get Basic Item
+        ItemStack item = createGuiItem(material, name, lore);
+
+        // Set Custom Head Data
+        SkullMeta meta = (SkullMeta) item.getItemMeta();
+
+
+
+
+
+        item.setItemMeta(meta);
+        return item;
+    }
+
+
+
+
     public void openInventory(final HumanEntity ent) {
         ent.openInventory(inv);
     }
@@ -72,22 +158,23 @@ public class LCPInventory implements Listener {
     // Check for clicks on items
     @EventHandler
     public void onInventoryClick(final InventoryClickEvent e) {
-        if (!e.getView().getTitle().equals("Leather Coloriser Pro")) {
-            return;
-        }
+        // Abort if the Inventory has the wrong name
+        if (!e.getView().getTitle().equals(name)) return;
+
+        // Abort only if the InventoryClick is within the created Inventory
+        if(e.getRawSlot() > size) return;
 
         e.setCancelled(true);
 
-        final ItemStack clickedItem = e.getCurrentItem();
 
         // verify current item is not null
-        if (clickedItem == null || clickedItem.getType().isAir()) {
-            return;
-        }
+        final ItemStack clickedItem = e.getCurrentItem();
+        if (clickedItem == null || clickedItem.getType().isAir()) return;
 
-        final Player p = (Player) e.getWhoClicked();
+
 
         // Using slots click is a best option for your inventory click's
+        final Player p = (Player) e.getWhoClicked();
         p.sendMessage("You clicked at slot " + e.getRawSlot());
     }
 
@@ -100,5 +187,5 @@ public class LCPInventory implements Listener {
         }
     }
 
-
+    // TODO: ITEMS CAN BE PLACED INSIDE THE GUI AND ARE GONE IF YOU CLOSE IT!
 }
